731cb6319c5e7e4a309677d86affb4736d3703a5
shivam <geetapandey595@gmail.com>
2017-12-22 23:36:42 +0530

    another one'

---
+++ Drawing.java
@@ -0,0 +1,10 @@
+/*<applet code ="Drawing" width="300" height = "100"> </applet>*/
+import java.awt.*;
+import java.applet.*;
+
+public class Drawing extends Applet{
+public void paint(Graphics g){
+
+g.drawString("i have nw used void paint function..",0,50);
+}
+}
