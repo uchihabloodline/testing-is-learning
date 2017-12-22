f0ea134c2e40274d06096630b04558756610bbc1
shivam <geetapandey595@gmail.com>
2017-12-22 22:53:54 +0530

    commiting again

---
+++ Aplet.java
@@ -0,0 +1,27 @@
+/*<applet code ="Aplet" width="300" height = "100"> </applet>*/
+
+import java.awt.*;
+
+import java.applet.*;
+
+class Aplet extends Applet {
+
+ public void init()
+ {
+  System.out.println("init function called..");
+
+ }
+
+ public void start(){
+ System.out.println(" start function called..");
+
+ }
+public void stop(){
+System.out.println("stop function called..");
+
+}
+public void destroy(){
+System.out.println("destroy function called..");
+}
+
+}
